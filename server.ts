badd12a0dd33d33f7c959483bd681dd07e6b59de
shihab-bhuiya <shihab.bhuiya07@gmail.com>
2026-08-27 10:53:24 +0600

    add config

--- server.ts
+++ server.ts
@@ -1,1 +1,12 @@
-console.log("Welcome to Note Book APIs");
+import app from "./src/app.js"
+
+const port =process.env.PORT || 5000
+
+const startServer = ()=>{
+    app.listen(port,()=>{
+        console.log(`Listening is running on :${port}`);
+    })
+}
+
+
+startServer()
