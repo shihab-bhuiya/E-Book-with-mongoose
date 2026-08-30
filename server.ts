39d53c503c67472e83ca8c6693f5814a87302076
shihab-bhuiya <shihab.bhuiya07@gmail.com>
2026-08-30 09:03:58 +0600

    setup golobal error

--- server.ts
+++ server.ts
@@ -1,12 +1,19 @@
-import app from "./src/app.js"
+import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
 
-const port =process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-const startServer = ()=>{
-    app.listen(port,()=>{
-        console.log(`Listening is running on :${port}`);
-    })
-}
+const startServer = async () => {
+  try {
+    await connectDB();
 
+    app.listen(port, () => {
+      console.log(`Listening is running on :${port}`);
+    });
+  } catch (error) {
+    console.log("Failed to start server:", error);
+    process.exit(1);
+  }
+};
 
-startServer()
+startServer();
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
-import express from "express";
+import express, { type NextFunction, type Request, type Response } from "express";
+
+import createHttpError from "http-errors";
+import globalErrorHandler from "./middleWare/glodbalErrorHandeler.js";
 
 const app = express();
 
 app.get('/',async(req,res)=>{
+
+    const error =   createHttpError(400,"Something went wrong");
+    throw error;
+
     res.json({
         success:true,
         message:"Server Running On 5000",
     })
 })
+
+app.use(globalErrorHandler)
 
 export default app;
