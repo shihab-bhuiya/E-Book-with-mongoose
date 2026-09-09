9263eda299f784f400407f25af18c23c5792e79a
shihab-bhuiya <shihab.bhuiya07@gmail.com>
2026-09-09 19:42:54 +0600

    create controller for book

---
+++ src/book/book.controller.ts
@@ -0,0 +1,25 @@
+import type { NextFunction, Request, Response } from "express";
+import BookModel from "./book.model.js";
+import createHttpError from "http-errors";
+
+const createBook = async (req: Request, res: Response,next: NextFunction) => {
+    try {
+        const { title, author, genre, coverImage, file } = req.body;
+
+        if (!title || !author || !genre || !coverImage || !file) {
+            return next(createHttpError(400, "All fields are required"));
+        }
+
+        const newBook = await BookModel.create({ title, author, genre, coverImage, file });
+
+        res.status(201).json({ message: "Book created successfully", book: newBook });
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+
+
+export { createBook };
--- src/book/book.router.ts
+++ src/book/book.router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { createBook } from "./book.controller.js";
 
 const bookRouter = Router();
 
 
+bookRouter.post("/books", createBook);
 
 
 export default bookRouter;
