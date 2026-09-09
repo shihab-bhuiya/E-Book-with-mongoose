6246c78360cd685a08f98be77c29759694d8fd29
shihab-bhuiya <shihab.bhuiya07@gmail.com>
2026-09-09 12:03:03 +0600

    Use try catch in user.controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,6 +7,7 @@ import bcyrpt from "bcrypt"
 
 import { config } from "../config/config.js";
 import jwt from "jsonwebtoken";
+import type { User } from "./user.types.js";
 
 const createUser = async(req:Request, res: Response, next: NextFunction)=>{
 
@@ -19,33 +20,50 @@ const createUser = async(req:Request, res: Response, next: NextFunction)=>{
         return next(error);
     }
 
+    try{ 
+
     const user = await userModel.findOne({email: email});
 
     if(user){
         const error = createHttpError(400,"User already exist");
         return next(error);
     }
+} catch(err){
+    const error = createHttpError(500,"Internal server error");
+    return next(error);
+}
 
 
 
     // password -> hash
 
+    let newUser : User;
+
+    try{
+
     const hashedPassword = await bcyrpt.hash(password, 10);
 
-    const newUser = await userModel.create({
+    newUser = await userModel.create({
         name,
         email,
         password:hashedPassword,
-    })
-
+    })  
+} catch(err){
+    const error = createHttpError(500,"Internal server error");
+    return next(error);
+}
+try{
 
     // token generation
 
-    const token = jwt.sign({sub: newUser._id}, config.JwtSecrect as string, {expiresIn: "7d"})
+    const token = jwt.sign({sub: newUser._id}, config.JwtSecrect as string, {expiresIn: "600"})
 
     res.status(200).json({
         accessToken: token,
-    })
+    })}
+catch(err){
+    const error = createHttpError(500,"Error while generating token");
+    return next(error);} 
 
 }
 
